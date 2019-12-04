301ee48b2fdc4962f311041ebe4892582289aa0f
RRoggia <renanzr@gmail.com>
2019-12-04 13:32:16 -0300

    apply changes to examples

--- spring-mvc-blocking-io/src/main/java/com/rroggia/spring/mvc/blocking/controller/RoutingController.java
+++ spring-mvc-blocking-io/src/main/java/com/rroggia/spring/mvc/blocking/controller/RoutingController.java
@@ -1,9 +1,5 @@
 package com.rroggia.spring.mvc.blocking.controller;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,24 +13,18 @@ import org.springframework.web.util.UriComponentsBuilder;
 @RestController
 public class RoutingController {
 
+	private static final String ENGINE_URL = "http://localhost:8081/route";
+
 	@GetMapping(path = "route", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public ResponseEntity<String> route(@RequestParam String delay) {
 
-		HttpHeaders headers = new HttpHeaders();
-		headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_UTF8_VALUE);
-		HttpEntity<String> entity = new HttpEntity<>(headers);
+		String uri = UriComponentsBuilder.fromHttpUrl(ENGINE_URL).queryParam("delay", delay).toUriString();
 
-		UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl("http://localhost:8081/route")
-				.queryParam("delay", delay);
-
-		ResponseEntity<String> exchange;
 		try {
-			exchange = new RestTemplate().exchange(uriBuilder.toUriString(), HttpMethod.GET, entity, String.class);
+			String response = new RestTemplate().getForObject(uri, String.class);
+			return ResponseEntity.ok().body(response);
 		} catch (HttpClientErrorException.BadRequest | HttpServerErrorException.InternalServerError e) {
-			return new ResponseEntity<>(e.getResponseBodyAsString(), HttpStatus.BAD_REQUEST);
+			return ResponseEntity.badRequest().body(e.getResponseBodyAsString());
 		}
-
-		return new ResponseEntity<>(exchange.getBody(), HttpStatus.OK);
 	}
-
 }
--- spring-mvc-non-blocking-io/src/main/java/com/rroggia/spring/mvc/non/blocking/controller/RoutingController.java
+++ spring-mvc-non-blocking-io/src/main/java/com/rroggia/spring/mvc/non/blocking/controller/RoutingController.java
@@ -1,27 +1,56 @@
 package com.rroggia.spring.mvc.non.blocking.controller;
 
+import java.util.concurrent.ForkJoinPool;
+
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.async.DeferredResult;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import reactor.core.publisher.Mono;
 
 @RestController
 public class RoutingController {
 
-	private static WebClient webClient = WebClient.create("http://localhost:8081");
+	private static final String ENGINE_URL = "http://localhost:8081";
+	private static final String ENGINE_WITH_ROUTE_ENDPOINT = "http://localhost:8081/route";
+	private static WebClient webClient = WebClient.create(ENGINE_URL);
 
 	@GetMapping(path = "route", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public Mono<String> route(@RequestParam String delay) {
 
-		return Mono.just(1).flatMap(i -> {
-			return webClient.get().uri("/route?delay=" + delay)
-					.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).retrieve()
-					.bodyToMono(String.class);
+		return webClient.get().uri("/route?delay=" + delay)
+				.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).retrieve()
+				.onStatus(HttpStatus::is4xxClientError, e -> Mono.error(new RuntimeException("e")))
+				.onStatus(HttpStatus::is5xxServerError, e -> Mono.error(new RuntimeException("e")))
+				.bodyToMono(String.class);
+
+	}
+
+	@GetMapping(path = "anotherRoute", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public DeferredResult<ResponseEntity<String>> anotherRoute(@RequestParam String delay) {
+		DeferredResult<ResponseEntity<String>> response = new DeferredResult<>();
+
+		ForkJoinPool.commonPool().submit(() -> {
+			String uri = UriComponentsBuilder.fromHttpUrl(ENGINE_WITH_ROUTE_ENDPOINT).queryParam("delay", delay).toUriString();
+
+			try {
+				response.setResult(ResponseEntity.ok().body(new RestTemplate().getForObject(uri, String.class)));
+			} catch (HttpClientErrorException.BadRequest | HttpServerErrorException.InternalServerError e) {
+				response.setResult(ResponseEntity.badRequest().body(e.getResponseBodyAsString()));
+			}
+
 		});
 
+		return response;
 	}
 }
